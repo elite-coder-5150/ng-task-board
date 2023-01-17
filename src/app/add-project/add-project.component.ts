cb74be0b9309fda0f3fcdff1facf1891f0bb8430
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-01-17 02:53:00 -0600

    logic for the component

--- src/app/add-project/add-project.component.ts
+++ src/app/add-project/add-project.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-add-project',
-  templateUrl: './add-project.component.html',
-  styleUrls: ['./add-project.component.scss']
+    selector: 'ng-add-project',
+    templateUrl: './add-project.component.html',
+    styleUrls: ['./add-project.component.scss']
 })
 export class AddProjectComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    onSubmit(value: any) {
+    }
 }
