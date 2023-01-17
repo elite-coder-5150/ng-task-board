fd4a99b16031ed69d9fa3d4b0d09d236a3a8f350
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-01-17 01:24:24 -0600

    all the new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SidebarNavComponent } from './sidebar-nav/sidebar-nav.component';
+import { ServicesComponent } from './services/services.component';
+import { SettingsComponent } from './settings/settings.component';
+import { AddProjectComponent } from './add-project/add-project.component';
+import { SecondaryHeaderComponent } from './secondary-header/secondary-header.component';
+import { FilterBarComponent } from './filter-bar/filter-bar.component';
+import { TaskBoardsComponent } from './task-boards/task-boards.component';
+import { TaskCardComponent } from './task-card/task-card.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchFormComponent,
+    ProfileComponent,
+    SidebarNavComponent,
+    ServicesComponent,
+    SettingsComponent,
+    AddProjectComponent,
+    SecondaryHeaderComponent,
+    FilterBarComponent,
+    TaskBoardsComponent,
+    TaskCardComponent,
+    NewTaskComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/task-boards/task-boards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-boards',
+  templateUrl: './task-boards.component.html',
+  styleUrls: ['./task-boards.component.scss']
+})
+export class TaskBoardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
